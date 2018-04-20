8c963a53964adb19964ec87a4643959a218ba635
Michael Cogswell <michael.a.cogswell@gmail.com>
2018-04-20 13:45:30 -0400

    fix vqa score computation

--- tools/compute_softscore.py
+++ tools/compute_softscore.py
@@ -78,16 +78,7 @@ punct = [';', r"/", '[', ']', '"', '{', '}',
 
 
 def get_score(occurences):
-    if occurences == 0:
-        return 0
-    elif occurences == 1:
-        return 0.3
-    elif occurences == 2:
-        return 0.6
-    elif occurences == 3:
-        return 0.9
-    else:
-        return 1
+    return min(1., occurences/3.)
 
 
 def process_punctuation(inText):
